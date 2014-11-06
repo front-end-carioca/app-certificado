cb27227ed657f304b24d8db2881e4f30595696c1
pedropolisenso <pedropolisenso@gmail.com>
2014-11-06 02:35:49 -0200

    starting new application

---
+++ conection/conexao.php
@@ -0,0 +1,5 @@
+<?php
+	// Conexão com o banco de dados
+	$conexao = mysql_connect('localhost', 'root', '');
+	$db      = mysql_select_db('DataBase_Name', $conexao);
+?>
---
+++ document.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+</head>
+<body>
+
+    <!-- Certificado a ser impresso -->
+    <div id="CertificadoPDF">
+        <div id="LogoPDF">
+            <img src="../assets/image/logotipo.jpg" alt="Front End Carioca">
+        </div><!-- LogoPDF END -->
+        <h1>certificado de participação</h1>
+        <p>
+            Declaramos para os devidos fins que,
+        </p>
+        <h2>{nome},</h2>
+        <p>
+            portador do RG: <span class="dado-rg">{rg}</span>
+        </p>
+        <p>
+            participou do Front End Carioca - RJ 2014 cumprindo uma carga horária total de 10 horas entre palestras e outras atividades.
+        </p>
+        <footer>
+            <p>Pedro Polisenso <br> Organizador do Front End Carioca</p>
+        </footer>
+    </div><!-- CertificadoPDF END -->
+</body>
+</html>
