c6c464d3d0ad6df8a151228e7097664583e36b8b
pedropolisenso <pedropolisenso@gmail.com>
2014-11-14 02:01:17 -0200

    File altered

--- conection/conexao.php
+++ conection/conexao.php
@@ -1,5 +1,5 @@
 <?php
 	// Conexão com o banco de dados
 	$conexao = mysql_connect('localhost', 'root', '');
-	$db      = mysql_select_db('NomeBancoDados', $conexao);
+	$db      = mysql_select_db('NomeDataBase', $conexao);
 ?>
